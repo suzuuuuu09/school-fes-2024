a7c868f5c9f6de6d3f088beb1ed6c229e3bc4d14
it0maki_ey <104641043+ikafurai19706@users.noreply.github.com>
2024-09-06 10:59:08 +0900

    refactoring

--- commenter.py
+++ commenter.py
@@ -1,25 +1,10 @@
 import tkinter as tk
+from time import sleep
 
-# ウィンドウの初期化
-root = tk.Tk()
-root.geometry("1920x1080")  # ウィンドウサイズを設定
-root.attributes("-transparentcolor", "white")  # 背景を透明にする色を設定
-root.attributes("-fullscreen", True)  # フルスクリーンに設定
-
-# キャンバスを作成してウィンドウに配置
-canvas = tk.Canvas(root, width=1920, height=1080, background="white", highlightthickness=0)
-canvas.place(x=0, y=0)
-
-default_x = 1920     # x座標
-default_y = 10     # y座標
-text_speed = 50     # テキストの動く速さ
-text = "あめんぼあかいなあいうえお"
-text_st = tk.StringVar()
-text_st.set(text)
-
-# ラベルを作成し、キャンバスに配置
-label1 = tk.Label(root, textvariable=text_st, font=('', 45), background="white", foreground="gray99")
-label1.place(x=default_x, y=default_y)
+MARGIN = 5000       # 余白
+default_x = 1920    # 開始位置のx座標
+default_y = 10      # 一行目のy座標
+text_speed = 1     # 流れるスピード
 
 def move():
     global default_x, text_speed
@@ -27,10 +12,33 @@ def move():
     label1.place(x=default_x, y=default_y)  # ラベルを再配置
     default_x -= text_speed
 
-    if default_x <= -canvas.winfo_width():  # 画面左端まで文字が到達した場合
+    if default_x <= -label1.winfo_reqwidth():  # 画面左端まで文字が到達した場合
         root.destroy()     # 終了する
 
-    root.after(100, move)  # 100ミリ秒ごとにスクロール
+    root.after(1, move)  # 1ミリ秒ごとにスクロール
+
+
+sleep(30)
+# ウィンドウの初期化
+root = tk.Tk()
+sWidth = root.winfo_width()
+sHeight = root.winfo_height()
+root.attributes("-transparentcolor", "white")  # 背景を透明にする色を設定
+root.attributes("-fullscreen", True)  # フルスクリーンに設定
+root.attributes("-topmost", True)   # ウィンドウを最前面に設定
+
+# キャンバスを作成してウィンドウに配置
+canvas = tk.Canvas(root, width=sWidth+MARGIN, height=sHeight+MARGIN, background="white")
+canvas.place(x=-(MARGIN/2), y=-(MARGIN/2))
+
+
+text = "あめんぼあかいなあいうえお"
+text_st = tk.StringVar()
+text_st.set(text)
+
+# ラベルを作成し、キャンバスに配置
+label1 = tk.Label(root, textvariable=text_st, font=('', 45), background="white", foreground="gray99")
+label1.place(x=default_x, y=default_y)
 
 # スクロール処理を開始
 move()
