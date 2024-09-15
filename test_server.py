da1681400b06317159bbb27effc4524112c6e5d8
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-09-15 17:35:51 +0900

    feat: json読み込み用のテスト鯖作成

---
+++ test_server.py
@@ -0,0 +1,15 @@
+from flask import *
+import json
+
+with open("test.json") as f:
+    data = json.load(f)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(port=8888)
