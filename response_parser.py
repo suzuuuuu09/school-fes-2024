e1f897a715dfc622c95ec3d4642e63fb516c0b39
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-09-18 17:07:24 +0900

    fix: 片方がNULLだったときのやつ書いてなかったので修正

--- response_parser.py
+++ response_parser.py
@@ -18,12 +18,19 @@ class ResponseParser:
         df_comment = pd.json_normalize(data['comment'])
         df_effect = pd.json_normalize(data['effect'])
 
-        if not df_comment.empty and not df_effect.empty:
-            min_df_comment = df_comment['timestamp'].min()
-            min_df_effect = df_effect['timestamp'].min()
+        if df_comment is not None and not df_comment.empty:
+           min_df_comment = df_comment['timestamp'].min()
+        else:
+            min_df_comment = None
 
+        if df_effect is not None and not df_effect.empty:
+            min_df_effect = df_effect['timestamp'].min()
+        else:
+            min_df_effect = None
+
+        if min_df_comment is not None and min_df_effect is not None:
             min_timestamp = min(min_df_comment, min_df_effect)
-            
+
             # timestampの差を計算
             df_comment['delay'] = (df_comment['timestamp'] - min_timestamp).fillna(0).astype(int)
             df_effect['delay'] = (df_effect['timestamp'] - min_timestamp).fillna(0).astype(int)
@@ -33,6 +40,10 @@ class ResponseParser:
             df_effect.drop(columns=['timestamp'], inplace=True)
 
             return df_comment, df_effect
+        elif min_df_comment is None and min_df_effect is not None:
+            return None, df_effect
+        elif min_df_effect is None and min_df_comment is not None:
+            return df_comment, None
         else:
             return None, None
 
@@ -46,7 +57,10 @@ class ResponseParser:
         return json.dumps(output_data, indent=2)
 
 if __name__ == '__main__':
-    url = 'https://yaovxdll6j.execute-api.ap-northeast-1.amazonaws.com/school-fes-2024-UnityRequestReceiver'
+    # prodで実行する場合
+    # url = 'https://yaovxdll6j.execute-api.ap-northeast-1.amazonaws.com/school-fes-2024-UnityRequestReceiver'
+    # devで実行する場合
+    url = 'http://localhost:8888/'
     response_interval = 5
     output_file_path = 'data/output.json'
 
