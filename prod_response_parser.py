b16003f0eaef84a5899c26089fdf45ed58d60c24
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-09-18 21:33:25 +0900

    update: update launch.json

---
+++ prod_response_parser.py
@@ -0,0 +1,24 @@
+from time import sleep
+from response_parser import ResponseParser
+
+# prodで実行する場合
+url = 'https://yaovxdll6j.execute-api.ap-northeast-1.amazonaws.com/school-fes-2024-UnityRequestReceiver'
+# devで実行する場合
+ # url = 'http://localhost:8888/'
+response_interval = 5
+output_file_path = 'data/output.json'
+
+while True:
+    parser = ResponseParser(url)
+    data = parser.get_data()
+    try:
+        df_comment, df_effect = parser.parse_data(data)
+    except Exception as e:
+        print(f"Error parsing data: {e}")
+        df_comment, df_effect = None, None    
+
+    json_data = parser.json_data(df_comment, df_effect)
+    with open(output_file_path, 'w') as f:
+        f.write(json_data)
+    print(json_data)
+    sleep(response_interval)
